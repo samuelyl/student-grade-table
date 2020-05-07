6c7888dc143dba213f96bfef0ec2b471b67bffbb
Sam Lee <samdagooner@gmail.com>
2020-05-06 23:05:23 -0700

    defined GradeForm class and added submit event listener to form plus updated App constructor parameters

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ var tableBody = document.querySelector(".table");
 var newGradeTable = new GradeTable (tableBody);
 var header = document.querySelector(".header");
 var newPageHeader = new PageHeader(header);
-var newApp = new App(newGradeTable, newPageHeader);
+var form = document.getElementById("main-form");
+var newGradeForm = new GradeForm(form);
+
+var newApp = new App(newGradeTable, newPageHeader, newGradeForm);
 
 newApp.start();
